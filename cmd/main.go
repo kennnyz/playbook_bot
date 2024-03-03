a29cdd7b3642b4796b14593379e185f103b9e076
kennnyz <muhammed1410@yandex.ru>
2024-03-04 01:37:13 +0300

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
-var users = make(map[int64]User)
+// Перечисление для состояний пользователя
+type UserState int
+
+const (
+	StateIdle UserState = iota
+	StateAwaitingSavePair
+	StateAwaitingDealPair
+	StateAwaitingBuyPrice
+	StateAwaitingSellPrice
+)
+
+// Мапа для отслеживания состояний пользователей
+var usersStates = make(map[int64]UserState)
+
+var users = make(map[int64]*User)
 
 type User struct {
-	UserPairs map[string]struct{}
-	UserDeals []*Deal
+	Name        string
+	UserPairs   map[string]struct{}
+	UserDeals   []*Deal
+	PendingDeal *Deal
 }
 
 type Deal struct {
@@ -33,7 +53,9 @@ func main() {
 
 	opts := []bot.Option{
 		bot.WithDefaultHandler(defaultHandler),
-		bot.WithCallbackQueryDataHandler("button", bot.MatchTypePrefix, callbackHandler),
+		bot.WithCallbackQueryDataHandler("/add_pair", bot.MatchTypeExact, addPair_callbackHandler),
+		bot.WithCallbackQueryDataHandler("/add_deal", bot.MatchTypeExact, addDeal_callbackHandler),
+		bot.WithCallbackQueryDataHandler("/get_history", bot.MatchTypeExact, getHistory_callbackHandler),
 		bot.WithMiddlewares(showMessageWithUserName),
 	}
 
@@ -42,68 +64,212 @@ func main() {
 		panic(err)
 	}
 
-	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, helloWorld)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, startCommand)
 
 	b.Start(ctx)
 }
 
-func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	//b.SendMessage(ctx, &bot.SendMessageParams{
-	//	ChatID: update.Message.Chat.ID,
-	//	Text:   update.Message.Text,
-	//})
-}
-
 func defaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	kb := &models.InlineKeyboardMarkup{
-		InlineKeyboard: [][]models.InlineKeyboardButton{
-			{
-				{Text: "Button 1", CallbackData: "button_1"},
-				{Text: "Button 2", CallbackData: "button_2"},
-			}, {
-				{Text: "Button 3", CallbackData: "button_3"},
-			},
-		},
-	}
+	chatID := getChatID(update)
+	currentState := usersStates[chatID]
 
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:      update.Message.Chat.ID,
-		Text:        "Click by button",
-		ReplyMarkup: kb,
-	})
+	switch currentState {
+	case StateAwaitingSavePair:
+		log.Println("saved")
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: chatID,
+			Text:   "Успешно сохранили вашу пару: " + update.Message.Text + " ✅",
+		})
+		users[getChatID(update)].UserPairs[update.Message.Text] = struct{}{}
+		usersStates[update.Message.Chat.ID] = StateIdle
+	case StateAwaitingDealPair:
+		handlePairSelection(ctx, b, update)
+	case StateAwaitingBuyPrice:
+		handleBuyPrice(ctx, b, update)
+	case StateAwaitingSellPrice:
+		handleSellPrice(ctx, b, update)
+	default:
+		showStandardButtons(ctx, b, update)
+	}
 }
 
-func callbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	// answering callback query first to let Telegram know that we received the callback query,
-	// and we're handling it. Otherwise, Telegram might retry sending the update repetitively
-	// as it thinks the callback query doesn't reach to our application. learn more by
-	// reading the footnote of the https://core.telegram.org/bots/api#callbackquery type.
+func addPair_callbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	usersStates[update.CallbackQuery.Message.Message.Chat.ID] = StateAwaitingSavePair
+	log.Printf("update user {%v} state for %v  ", update.CallbackQuery.Message.Message.Chat.Username, StateAwaitingSavePair)
+
 	b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
 		CallbackQueryID: update.CallbackQuery.ID,
 		ShowAlert:       false,
 	})
+
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.CallbackQuery.Message.Message.Chat.ID,
-		Text:   "You selected the button: " + update.CallbackQuery.Data,
-	})
-
-	// Remove the buttons by editing the message's reply markup
-	b.EditMessageReplyMarkup(ctx, &bot.EditMessageReplyMarkupParams{
-		ChatID:    update.CallbackQuery.Message.Message.Chat.ID,
-		MessageID: update.CallbackQuery.Message.Message.ID,
-		ReplyMarkup: &models.InlineKeyboardMarkup{
-			InlineKeyboard: [][]models.InlineKeyboardButton{},
-		},
+		Text:   "Введите название актива/пары (напр. Amazon, BTC/USDT): ",
 	})
 }
 
-func addDealCommand(ctx context.Context, b *bot.Bot, update *models.Update) {
+func addDeal_callbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	userChatID := getChatID(update)
+	if userChatID == 0 {
+		return
+	}
 
+	// Получаем пользователя
+	user, ok := users[userChatID]
+	if !ok {
+		log.Println("User not found")
+		return
+	}
+	log.Println("adding deal to ", user.Name)
+
+	// Проверяем, есть ли у пользователя пары
+	if len(user.UserPairs) == 0 {
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.CallbackQuery.Message.Message.Chat.ID,
+			Text:   "У вас пока нет ни одной пары для добавления сделки. Вы можете добавить их с помощью кнопки 'Добавить пару'.",
+		})
+		return
+	}
+
+	// Формируем клавиатуру с кнопками пар пользователя
+	var keyboard [][]models.InlineKeyboardButton
+	for pair := range user.UserPairs {
+		keyboard = append(keyboard, []models.InlineKeyboardButton{{Text: pair, CallbackData: pair}})
+	}
+
+	// Отправляем сообщение с клавиатурой
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      userChatID,
+		Text:        "Выберите пару для добавления сделки:",
+		ReplyMarkup: models.InlineKeyboardMarkup{InlineKeyboard: keyboard},
+	})
+	usersStates[userChatID] = StateAwaitingDealPair
 }
 
-// addPairCommand adding new pair to the list
-func addPairCommand(ctx context.Context, b *bot.Bot, update *models.Update) {
+func handlePairSelection(ctx context.Context, b *bot.Bot, update *models.Update) {
+	userChatID := getChatID(update)
+	if userChatID == 0 {
+		return
+	}
 
+	// Получаем пользователя
+	user, ok := users[userChatID]
+	if !ok {
+		log.Println("User not found")
+		return
+	}
+
+	// Получаем выбранную пользователем пару
+	selectedPair := update.CallbackQuery.Data
+
+	// Устанавливаем выбранную пару во временную сделку
+	user.PendingDeal = &Deal{Pair: selectedPair}
+
+	// Запрашиваем цену покупки
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: userChatID,
+		Text:   "Укажите цену покупки:",
+	})
+
+	// Устанавливаем состояние пользователя в ожидание ввода первого поля сделки (BuyPrice)
+	usersStates[userChatID] = StateAwaitingBuyPrice
+}
+
+func handleBuyPrice(ctx context.Context, b *bot.Bot, update *models.Update) {
+	userChatID := getChatID(update)
+	if userChatID == 0 {
+		return
+	}
+
+	// Получаем пользователя
+	user, ok := users[userChatID]
+	if !ok {
+		log.Println("User not found")
+		return
+	}
+
+	buyPrice, err := strconv.ParseFloat(update.Message.Text, 64)
+	if err != nil {
+		log.Println("Error parsing buy price:", err)
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: userChatID,
+			Text:   "невалидный формат",
+		})
+		return
+	}
+
+	user.PendingDeal.BuyPrice = buyPrice
+
+	// Запрашиваем цену продажи
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: userChatID,
+		Text:   "Укажите цену продажи:",
+	})
+	// Устанавливаем состояние пользователя в ожидание ввода цены продажи
+	usersStates[userChatID] = StateAwaitingSellPrice
+}
+
+func handleSellPrice(ctx context.Context, b *bot.Bot, update *models.Update) {
+	userChatID := getChatID(update)
+	if userChatID == 0 {
+		return
+	}
+
+	// Получаем пользователя
+	user, ok := users[userChatID]
+	if !ok {
+		log.Println("User not found")
+		return
+	}
+
+	// Получаем цену покупки
+	update.Message.Text = strings.Replace(update.Message.Text, ",", ".", -1)
+	sellPrice, err := strconv.ParseFloat(update.Message.Text, 64)
+	if err != nil {
+		log.Println("Error parsing buy price:", err)
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: userChatID,
+			Text:   "невалидный формат",
+		})
+		return
+	}
+
+	// Записываем цену покупки в структуру сделки
+	user.PendingDeal.SellPrice = sellPrice
+
+	// Устанавливаем состояние пользователя в ожидание ввода даты
+	completeDeal(ctx, b, userChatID, user)
+	showStandardButtons(ctx, b, update)
+	usersStates[userChatID] = StateIdle
+}
+
+func completeDeal(ctx context.Context, b *bot.Bot, userChatID int64, user *User) {
+	user.PendingDeal.Profit = user.PendingDeal.SellPrice - user.PendingDeal.BuyPrice
+	user.PendingDeal.ProfitPercent = truncateFloat((user.PendingDeal.Profit/user.PendingDeal.BuyPrice)*100, 2)
+
+	user.PendingDeal.Date = time.Now()
+
+	user.UserDeals = append(user.UserDeals, user.PendingDeal)
+	dealText := "<b> Сделка успешно добавлена 🎉 Ваша сделка:</b>\n" +
+		"<b>Покупка:</b> " + strconv.FormatFloat(user.PendingDeal.BuyPrice, 'f', 2, 64) + "\n" +
+		"<b>Продажа:</b> " + strconv.FormatFloat(user.PendingDeal.SellPrice, 'f', 2, 64) + "\n" +
+		"<b>Прибыль:</b> " + strconv.FormatFloat(user.PendingDeal.Profit, 'f', 2, 64) + "\n" +
+		"<b>Процент прибыли:</b> " + strconv.FormatFloat(user.PendingDeal.ProfitPercent, 'f', 2, 64) + "%\n"
+	fmt.Printf("%s deal: \nbuy price %v\nsell price %v \nprofit %v\nprofit percentage %v\n", user.Name, user.PendingDeal.BuyPrice, user.PendingDeal.SellPrice, user.PendingDeal.Profit, user.PendingDeal.ProfitPercent)
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    userChatID,
+		Text:      dealText,
+		ParseMode: "HTML",
+	})
+	user.PendingDeal = nil
+}
+
+func getHistory_callbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.CallbackQuery.Message.Message.Chat.ID,
+		Text:   "пока не реализованная функция 🙈",
+	})
+	showStandardButtons(ctx, b, update)
 }
 
 func showMessageWithUserName(next bot.HandlerFunc) bot.HandlerFunc {
@@ -115,10 +281,68 @@ func showMessageWithUserName(next bot.HandlerFunc) bot.HandlerFunc {
 	}
 }
 
-func helloWorld(ctx context.Context, b *bot.Bot, update *models.Update) {
+func startCommand(ctx context.Context, b *bot.Bot, update *models.Update) {
 	message := "Привет 👋\nЭто Бот с помощью которого можно вести учет ваших сделок📖\n\nПоддерживаемые команды:\n/add_deal - добавить новую сделку\n/get_history - получить историю сделок"
-	b.SendMessage(ctx, &bot.SendMessageParams{
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   message,
 	})
+	if err != nil {
+		log.Printf("can't send message to %s, error : %v", update.Message.Chat.Username, err)
+	}
+
+	if _, ok := users[update.Message.Chat.ID]; !ok {
+		var deals []*Deal
+		users[update.Message.Chat.ID] = &User{
+			Name:      update.Message.Chat.Username,
+			UserPairs: make(map[string]struct{}),
+			UserDeals: deals,
+		}
+
+		log.Println("Saved new user: ", update.Message.Chat.Username)
+	}
+
+	showStandardButtons(ctx, b, update)
+}
+
+func showStandardButtons(ctx context.Context, b *bot.Bot, update *models.Update) error {
+	kb := &models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "Добавить сделку", CallbackData: "/add_deal"},
+				{Text: "Добавить пару", CallbackData: "/add_pair"},
+			},
+			{
+				{Text: "История сделок", CallbackData: "/get_history"},
+			},
+		},
+	}
+
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      getChatID(update),
+		Text:        "Выберите действие",
+		ReplyMarkup: kb,
+	})
+	if err != nil {
+		log.Printf("can't send message to %s, error : %v", update.Message.Chat.Username, err)
+		return err
+	}
+
+	return nil
+}
+
+func getChatID(update *models.Update) int64 {
+	if update.Message != nil {
+		return update.Message.Chat.ID
+	} else if update.CallbackQuery != nil {
+		return update.CallbackQuery.Message.Message.Chat.ID
+	}
+
+	return 0
+}
+
+func truncateFloat(f float64, precision int) float64 {
+	multiplier := math.Pow(10, float64(precision))
+	truncated := math.Trunc(f*multiplier) / multiplier
+	return truncated
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module playbook_bot
 
 go 1.21.7
 
-require github.com/go-telegram/bot v1.1.4 // indirect
+require github.com/go-telegram/bot v1.1.4
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
